b0b04b447c6b927838600485ac117584efd1cf44
skyworkerxsky <pwuser@yandex.ru>
2022-10-09 01:52:34 +0500

    topic links

--- project/learning_log/learning_logs/urls.py
+++ project/learning_log/learning_logs/urls.py
@@ -7,4 +7,8 @@ app_name = 'learning_logs'
 urlpatterns = [
       # Домашняя страница
       path('', views.index, name='index'),
+      # Список тем
+      path('topics', views.topics, name='topics'),
+      # Страница с подробной информацией по отдельной теме
+      path('topics/<int:topic_id>/', views.topic, name='topic')
 ]
--- project/learning_log/learning_logs/views.py
+++ project/learning_log/learning_logs/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from .models import Topic
 
 # Create your views here.
 
 def index(request):
-    """Домашняя страница приложения Learning Log"""
+    """ Домашняя страница приложения Learning Log """
     return render(request, 'learning_logs/index.html')
+
+def topics(request):
+    """ Выводит список тем. """
+    topics = Topic.objects.order_by('date_added')
+    context = {'topics': topics}
+    return render(request, 'learning_logs/topics.html', context)
+
+def topic(request, topic_id):
+    """ Выводит тему и все её записи """
+    topic = Topic.objects.get(id=topic_id) # функция get() используется для получения темы
+    entries = topic.entry_set.order_by('-date_added') # Загружаются записи, связанные с данной темой, и они упорядочиваются по значению date_added: знак «минус» перед date_added сортирует результаты в обратном порядке,
+    context = {'topic': topic, 'entries': entries} # Тема и записи сохраняются в словаре context
+    return render(request, 'learning_logs/topic.html', context)
