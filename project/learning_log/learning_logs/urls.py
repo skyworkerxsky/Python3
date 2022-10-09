32f208820b72f8e7f9b5078f5c9d7d268a655c3c
skyworkerxsky <pwuser@yandex.ru>
2022-10-10 00:22:25 +0500

    create topic, entry

--- project/learning_log/learning_logs/urls.py
+++ project/learning_log/learning_logs/urls.py
@@ -10,5 +10,9 @@ urlpatterns = [
       # Список тем
       path('topics', views.topics, name='topics'),
       # Страница с подробной информацией по отдельной теме
-      path('topics/<int:topic_id>/', views.topic, name='topic')
+      path('topics/<int:topic_id>/', views.topic, name='topic'),
+      # Страница для добавления новой темы
+      path('new_topic/', views.new_topic, name='new_topic'),
+      # Станица для добавления новой записи
+      path('new_entry/<int:topic_id>/', views.new_entry, name='new_entry'), # Код <int:topic_id> захватывает числовое значение и сохраняет его в переменной topic_id
 ]
--- project/learning_log/learning_logs/views.py
+++ project/learning_log/learning_logs/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Topic
+from .forms import TopicForm, EntryForm
 
 # Create your views here.
 
@@ -19,3 +20,38 @@ def topic(request, topic_id):
     entries = topic.entry_set.order_by('-date_added') # Загружаются записи, связанные с данной темой, и они упорядочиваются по значению date_added: знак «минус» перед date_added сортирует результаты в обратном порядке,
     context = {'topic': topic, 'entries': entries} # Тема и записи сохраняются в словаре context
     return render(request, 'learning_logs/topic.html', context)
+
+def new_topic(request):
+    """ Определяет новую тему. """
+    if request.method != 'POST':
+        # Данные не отправлялись, создается пустая форма
+        form = TopicForm()
+    else:
+        # Отправлены данные POST, обработать данные
+        form = TopicForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('learning_logs:topics')
+
+    # Вывести пустую или недействительную форму
+    context = {'form': form}
+    return render(request, 'learning_logs/new_topic.html', context)
+
+def new_entry(request, topic_id):
+    """ Добавляет новую запись по конкретной теме. """
+    topic = Topic.objects.get(id=topic_id)
+    if request.method != 'POST':
+        # Данные не отправлялись; создается пустая форма.
+        form = EntryForm()
+    else:
+        # Отправлены данные POST; обработать данные.
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            return redirect('learning_logs:topic', topic_id=topic_id)
+
+    # Вывести пустую или недействительную форму
+    context = {'topic': topic, 'form': form}
+    return render(request, 'learning_logs/new_entry.html', context)
